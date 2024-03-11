6867f814fc400ac784cc8dbeccd4f2b7e2a7bb5f
Kevin Lai <clai@ucsb.edu>
2024-03-11 15:28:41 -0700

    fix viz

--- viz.py
+++ viz.py
@@ -2,11 +2,13 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 from MBMST import parse, find_MBMST
+from MAMST import find_MAMST
 
 class Visualization:
     def __init__(self, V, frames, interval):
         self.V = V
         self.MBMST = find_MBMST(V)
+        # self.MBMST = find_MAMST(V)
         self.frames = frames
         self.interval = interval
 
@@ -14,11 +16,23 @@ class Visualization:
         fig, ax = plt.subplots()
         nodes = ax.scatter(*zip(*self.V.keys()))
         edges = { e : ax.plot([e[0][0], e[1][0]], [e[0][1], e[1][1]], color="C0")[0] for e in self.MBMST }
-        ax.set(xlim=[-10, 10], ylim=[-10, 10])
-        mindim = min(abs(ax.get_xlim()[1] - ax.get_xlim()[0]), abs(ax.get_ylim()[1] - ax.get_ylim()[0]))
-        div = mindim / self.frames
+        def calc_lims(c, v):
+            cx, cy = c
+            vx, vy = v
+            cx0, cx1 = cx, cx + vx * self.frames
+            cy0, cy1 = cy, cy + vy * self.frames
+            min_x, max_x = (cx0, cx1) if cx0 < cx1 else (cx1, cx0)
+            min_y, max_y = (cy0, cy1) if cy0 < cy1 else (cy1, cy0)
+            return ([min_x, max_x], [min_y, max_y])
+        x_lim, y_lim = calc_lims(*list(self.V.items())[0])
+        for c, v in self.V.items():
+            x_lim_, y_lim_ = calc_lims(c, v)
+            for lim, lim_ in [(x_lim, x_lim_), (y_lim, y_lim_)]:
+                lim[0] = min(lim[0], lim_[0])
+                lim[1] = max(lim[1], lim_[1])
+        ax.set(xlim=x_lim, ylim=y_lim)
         def update(frame):
-            new_coords = { cv[0] : np.array(cv[0]) + np.array(cv[1]) * frame * div for cv in self.V.items() }
+            new_coords = { cv[0] : np.array(cv[0]) + np.array(cv[1]) * frame for cv in self.V.items() }
             for e, e_plt in edges.items():
                 e_plt.set_xdata([new_coords[e[0]][0], new_coords[e[1]][0]])
                 e_plt.set_ydata([new_coords[e[0]][1], new_coords[e[1]][1]])
@@ -32,5 +46,5 @@ class Visualization:
 
 if __name__ == "__main__":
     V = parse()
-    viz = Visualization(V, 1000, 1)
+    viz = Visualization(V, 500, .01)
     viz.run()
