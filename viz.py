d06ee847b3a69982b08fbfc2d753acb0b60aa37a
Kevin Lai <clai@ucsb.edu>
2024-03-13 17:17:08 -0700

    viz

--- viz.py
+++ viz.py
@@ -14,7 +14,7 @@ class Visualization:
 
     def run(self):
         fig, ax = plt.subplots()
-        nodes = ax.scatter(*zip(*self.V.keys()))
+        nodes = ax.scatter(*zip(*map(lambda pv: pv[:2], V)))
         edges = { e : ax.plot([e[0][0], e[1][0]], [e[0][1], e[1][1]], color="C0")[0] for e in self.MBMST }
         def calc_lims(c, v):
             cx, cy = c
@@ -24,15 +24,15 @@ class Visualization:
             min_x, max_x = (cx0, cx1) if cx0 < cx1 else (cx1, cx0)
             min_y, max_y = (cy0, cy1) if cy0 < cy1 else (cy1, cy0)
             return ([min_x, max_x], [min_y, max_y])
-        x_lim, y_lim = calc_lims(*list(self.V.items())[0])
-        for c, v in self.V.items():
-            x_lim_, y_lim_ = calc_lims(c, v)
+        x_lim, y_lim = calc_lims(list(self.V)[0][:2], list(self.V)[0][2:])
+        for cv in self.V:
+            x_lim_, y_lim_ = calc_lims(cv[:2], cv[2:])
             for lim, lim_ in [(x_lim, x_lim_), (y_lim, y_lim_)]:
                 lim[0] = min(lim[0], lim_[0])
                 lim[1] = max(lim[1], lim_[1])
         ax.set(xlim=x_lim, ylim=y_lim)
         def update(frame):
-            new_coords = { cv[0] : np.array(cv[0]) + np.array(cv[1]) * frame for cv in self.V.items() }
+            new_coords = { cv : np.array(cv[:2]) + np.array(cv[2:]) * frame for cv in self.V }
             for e, e_plt in edges.items():
                 e_plt.set_xdata([new_coords[e[0]][0], new_coords[e[1]][0]])
                 e_plt.set_ydata([new_coords[e[0]][1], new_coords[e[1]][1]])
