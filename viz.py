1bbcaa62c2b82fe87c735093392ad4075833de17
Kevin Lai <clai@ucsb.edu>
2024-03-13 17:37:02 -0700

    viz computes cost

--- viz.py
+++ viz.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
+import numpy.linalg as npl
 from MBMST import parse, find_MBMST
 from MAMST import find_MAMST
 
@@ -13,9 +14,13 @@ class Visualization:
         self.interval = interval
 
     def run(self):
-        fig, ax = plt.subplots()
-        nodes = ax.scatter(*zip(*map(lambda pv: pv[:2], V)))
-        edges = { e : ax.plot([e[0][0], e[1][0]], [e[0][1], e[1][1]], color="C0")[0] for e in self.MBMST }
+        fig, ax = plt.subplots(2, 1)
+        plt.subplots_adjust(hspace=.5)
+        nodes = ax[0].scatter(*zip(*map(lambda pv: pv[:2], V)))
+        ax[0].set_title("Points in the plane")
+        edges = { e : ax[0].plot([e[0][0], e[1][0]], [e[0][1], e[1][1]], color="C0")[0] for e in self.MBMST }
+        cost_map = {}
+        cost = ax[1].plot(list(cost_map.keys()), list(cost_map.values()))[0]
         def calc_lims(c, v):
             cx, cy = c
             vx, vy = v
@@ -30,25 +35,33 @@ class Visualization:
             for lim, lim_ in [(x_lim, x_lim_), (y_lim, y_lim_)]:
                 lim[0] = min(lim[0], lim_[0])
                 lim[1] = max(lim[1], lim_[1])
-        ax.set(xlim=x_lim, ylim=y_lim)
+        ax[0].set(xlim=x_lim, ylim=y_lim)
         def update(frame):
             new_coords = { cv : np.array(cv[:2]) + np.array(cv[2:]) * frame for cv in self.V }
+            curr_cost = .0
             for e, e_plt in edges.items():
                 e_plt.set_xdata([new_coords[e[0]][0], new_coords[e[1]][0]])
                 e_plt.set_ydata([new_coords[e[0]][1], new_coords[e[1]][1]])
+                curr_cost += npl.norm(new_coords[e[1]] - new_coords[e[0]])
+            cost_map[frame] = curr_cost
+            cost.set_data(*zip(*cost_map.items()))
+            min_cost = min(cost_map.values())
+            max_cost = max(cost_map.values())
+            cost.axes.axis([0, frame, 1.2 * (min_cost - max_cost) + max_cost, max_cost])
+            ax[1].set_title("MBMST cost = {:.3f}".format(curr_cost))
             new_x = list(map(lambda nc: nc[0], new_coords.values()))
             new_y = list(map(lambda nc: nc[1], new_coords.values()))
             data = np.stack([new_x, new_y]).T
             nodes.set_offsets(data)
-            return (nodes, edges)
+            return (nodes, edges, cost)
         ani = animation.FuncAnimation(fig=fig, func=update, frames=self.frames, interval=self.interval, repeat=True)
         def save_ani():
             writer = animation.PillowWriter(fps=10,
                                             metadata=dict(artist='Me'),
                                             bitrate=2)
             ani.save('scatter.gif', writer=writer)
-        # save_ani()
-        plt.show()
+        save_ani()
+        # plt.show()
 
 if __name__ == "__main__":
     V = parse()
